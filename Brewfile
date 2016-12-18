198652011625116a75abe743ef12c8562c121912
Erik Verstegen <erik@flowingbytes.nl>
2016-12-18 17:27:34 +0100

    Add a Brewfile

---
+++ Brewfile
@@ -0,0 +1,14 @@
+cask_args appdir: '/Applications'
+
+tap 'caskroom/cask'
+
+brew 'ack'
+brew 'tree'
+brew 'wget'
+
+brew 'git'
+brew 'zsh'
+brew 'tmux'
+brew 'reattach-to-user-namespace'
+brew 'vim', args: ['with-override-system-vi']
+
--- install/darwin/homebrew.sh
+++ install/darwin/homebrew.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-install() {
-    # Test if the requested package is not installed.
-    if ! brew ls --versions "${1}" > /dev/null; then
-        info "Installing \"${1}\"..."
-
-        brew install "${1}"
-
-        success "Finished installing \"${1}\"."
-    fi
-}
-
 # Test if Homebrew is not installed.
 if test ! $(which brew); then
     info "Installing Homebrew..."
@@ -22,14 +11,7 @@ fi
 
 info "Installing Homebrew packages..."
 
-install ack
-install tree
-install wget
-
-install git
-install zsh
-install tmux
-install reattach-to-user-namespace
-install vim --with-override-system-vi
+brew tap homebrew/bundle
+brew bundle
 
 success "Finished installing Homebrew packages."
