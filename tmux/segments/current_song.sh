2eaaba346176cc390f3c8e13a59def9810722d43
Erik Verstegen <erik@flowingbytes.nl>
2017-01-03 16:36:08 +0100

    Improve the radio song detection

--- tmux/segments/current_song.sh
+++ tmux/segments/current_song.sh
@@ -3,32 +3,27 @@
 # Inspired by: https://www.tylerewing.co/tmux-now-playing
 
 osascript <<APPLESCRIPT
-(* Test if Safari is running. *)
-if is_app_running("Safari") then
-    tell application "Safari"
-        (* Get all the open URLs. *)
-        set urls to get URL of every tab of every window
-
-        (* Loop through all the open URLs. *)
-        repeat with currentWindow in urls
-            repeat with currentUrl in currentWindow
-                (* Test if we are listening to Radio 2. *)
-                if (currentUrl as string) is equal to "http://radioplayer.npo.nl/radio2/" then
-                    return do shell script "~/.dotfiles/tmux/segments/radio2.sh"
-                end if
-            end repeat
-        end repeat
-    end tell
-end if
-
 (* Test if iTunes is running. *)
 if is_app_running("iTunes") then
     tell application "iTunes"
         if (player state as text) is equal to "playing" then
-            set artist_name to artist of current track
-            set track_name to name of current track
+            if current stream title is not equal to missing value then
+                set song_information to my extract_song_information(get current stream title)
 
-            return "#[fg=colour246]♫ " & track_name & " - #[bold]" & artist_name
+                if item 1 of song_information starts with "NPO Radio" or length of song_information is equal to 1 then
+                    return
+                end if
+
+                set artist_name to item 1 of song_information
+                set track_name to item 2 of song_information
+
+                return "#[fg=colour246]♫ " & artist_name & " - #[bold]" & track_name
+            else
+                set artist_name to artist of current track
+                set track_name to name of current track
+
+                return "#[fg=colour246]♫ " & artist_name & " - #[bold]" & track_name
+            end if
         end if
     end tell
 end if
@@ -49,4 +44,13 @@ end if
 on is_app_running(app_name)
     tell application "System Events" to (name of processes) contains app_name
 end is_app_running
+
+(* Extract song information from radio streams. *)
+on extract_song_information(current_song)
+    set AppleScript's text item delimiters to " - "
+    set song_information to current_song's text items
+    set AppleScript's text item delimiters to {""}
+
+    return song_information
+end extract_song_information
 APPLESCRIPT
