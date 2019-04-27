980a754988d801f8cdb560dc8c7959fd1ef4a43b
Erik Verstegen <erik@flowingbytes.nl>
2019-04-27 15:20:11 +0200

    Make the PATH variable more easily configurable

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -8,10 +8,12 @@ export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
 # Set the PATH variable.
-export PATH=$HOME/.dotfiles/bin
-export PATH=/usr/local/go/bin:$PATH
-export PATH=/usr/local/sbin:$PATH
-export PATH=/usr/local/bin:$PATH
-export PATH=/usr/sbin:$PATH
-export PATH=/usr/bin:$PATH
-export PATH=/bin:$PATH
+PATHS=(
+    "/usr/local/bin"
+    "/usr/bin"
+    "/bin"
+    "/usr/sbin"
+    "/sbin"
+)
+
+export PATH="${(j.:.)PATHS}"
