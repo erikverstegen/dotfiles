e75041ba2bfa4b2b6854834ba4080c0336c32837
Erik Verstegen <hello@erikverstegen.com>
2022-06-08 11:30:28 +0200

    fix(tmux): use "Music" as the music application instead of "iTunes"

--- tmux/segments/current_song.sh
+++ tmux/segments/current_song.sh
@@ -6,13 +6,13 @@ tell application "System Events"
     set running_apps to (name of every process)
 end tell
 
-set app_names to {"iTunes", "Spotify"}
+set app_names to {"Music"}
 
 repeat with app_name in app_names
     (* Test if the app is running. *)
     if running_apps contains app_name then
         tell application app_name
-            using terms from application "iTunes"
+            using terms from application "Music"
                 if (player state as text) is equal to "playing" then
                     set artist_name to artist of current track
                     set track_name to name of current track
